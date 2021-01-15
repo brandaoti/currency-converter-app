ecac9affd94137fe2dcbbe53112b2325331aacb8
Daniel Brandão <dannnbrandao@gmail.com>
2021-01-14 21:43:38 -0300

    add image.network pra testar instancia da imagem

--- lib/app/views/home_view.dart
+++ lib/app/views/home_view.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
+  // * link com imagem provisoria
+  final String img =
+      "https://imagensemoldes.com.br/wp-content/uploads/2020/04/money-png.png";
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Image.network(
+            img,
+          ),
+        ],
+      ),
+    );
   }
 }
