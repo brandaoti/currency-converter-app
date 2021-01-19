8ad951367e6c20947bbdb76f2a8ef5298fcb81ed
Daniel Brandão <dannnbrandao@gmail.com>
2021-01-19 03:23:12 -0300

    add icon com retorno de imagem e customização

--- lib/app/components/custom_textfield.dart
+++ lib/app/components/custom_textfield.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
+  // Variaveis
+  final String name;
+  final String image;
+
+  // Construtor
+  CustomTextField({
+    this.image,
+    @required this.name,
+  });
+
+  // ! Add Icon com imagem
+  // ! Add um Hint ou não
+  // !
+
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        labelText: "Real exemplo",
+    //
+    Widget _iconImg =
+        Image.asset("assets/images/real.png", scale: 2.5, color: Colors.green);
+
+    return Container(
+      color: Colors.amber,
+      //padding: const EdgeInsets.all(4.0),
+      child: TextField(
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          icon: _iconImg,
+          labelText: name,
+        ),
       ),
     );
   }
